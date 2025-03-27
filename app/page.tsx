dac61635b220d4cb17b0491de36a8548d25e3ed9
anantakoirala <ananta.drn2049@gmail.com>
2025-03-27 11:03:11 +0545

    theme switcher

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import ThemeSwitcher from "@/components/ThemeSwitcher";
 import { Button } from "@/components/ui/button";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
@@ -5,6 +6,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
+      <ThemeSwitcher />
       <h1>This is homepage</h1>
       <p>This is page content</p>
     </div>
